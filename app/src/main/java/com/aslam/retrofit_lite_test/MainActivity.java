2a2df1b0d68db147c22b21489710be048b66a219
Aslam Anver <slam.anver9@gmail.com>
2020-02-10 17:33:03 +0530

    version

--- app/src/main/java/com/aslam/retrofit_lite_test/MainActivity.java
+++ app/src/main/java/com/aslam/retrofit_lite_test/MainActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.aslam.retrofit_lite.services.APITask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        APITask apiTask = APITask.from(this);
+        SampleReq req = new SampleReq();
+        req.email = "aslam";
+        apiTask.sendPOST(101, "https://reqres.in/api/login", req, null, null);
     }
 }
