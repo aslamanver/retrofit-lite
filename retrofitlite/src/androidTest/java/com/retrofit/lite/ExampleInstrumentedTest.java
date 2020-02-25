73812b46135c9cdf63c384bafe32d49eca29daa2
Aslam Anver <slam.anver9@gmail.com>
2020-02-25 15:07:08 +0530

    Bintray version - CI tested

--- retrofitlite/src/androidTest/java/com/retrofit/lite/ExampleInstrumentedTest.java
+++ retrofitlite/src/androidTest/java/com/retrofit/lite/ExampleInstrumentedTest.java
@@ -1,21 +1,12 @@
 package com.retrofit.lite;
 
 import android.content.Context;
-import android.util.Log;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.aslam.retrofit_lite.services.APIClient;
-import com.aslam.retrofit_lite.services.APITask;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.Map;
-
-import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
