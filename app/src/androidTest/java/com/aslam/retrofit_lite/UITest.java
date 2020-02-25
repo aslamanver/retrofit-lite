d02e75d26b14531504dc03dca10590534b0998a2
Aslam Anver <slam.anver9@gmail.com>
2020-02-25 15:21:21 +0530

    Bintray version - CI tested

--- app/src/androidTest/java/com/aslam/retrofit_lite/UITest.java
+++ app/src/androidTest/java/com/aslam/retrofit_lite/UITest.java
@@ -4,8 +4,13 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -15,22 +20,22 @@ public class UITest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
-    // @Before
-    // public void before() {
-    //
-    //     Tester.setMaxWaitTime(1000 * 60);
-    // }
-    //
-    // @Test
-    // public void test_emv_with_key_entry_sale() {
-    //
-    // }
-    //
-    // @After
-    // public void after() {
-    //
-    //     Tester.sleep(3000);
-    //     Tester.resetMaxWaitTime();
-    // }
+    @Before
+    public void before() {
+
+        Tester.setMaxWaitTime(1000 * 60);
+    }
+
+    @Test
+    public void test_samle() {
+        assertTrue(true);
+    }
+
+    @After
+    public void after() {
+
+        Tester.sleep(3000);
+        Tester.resetMaxWaitTime();
+    }
 
 }
